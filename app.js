686b9b1bf1c8fbc28dd27719d21b177f31eb3b18
Yout <youtlone95@gmail.com>
2024-01-11 20:29:24 +0630

    create event emitter for class

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-const tutorial = require("./tutorial");
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
-console.log(tutorial.sum(1,2));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
+eventEmitter.on('tutorial', (num1,num2) => {
+    const total = num1 + num2;
+    console.log(total);
+})
+
+eventEmitter.emit('tutorial',1,3);
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super();
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
+
+let jame = new Person('Jame');
+let julia = new Person('Julia');
+
+julia.on('name', () => {
+    console.log("My name is " + julia.name)
+})
+jame.on('name', () => {
+    console.log('My name is ' + jame.name);
+});
+
+julia.emit('name');
+jame.emit('name');
